efe42c8a4c8a523b894e1525aea1c003ba3fce57
João Pedro Paz <77215433+JoaoPedroPaz@users.noreply.github.com>
2024-07-12 16:23:12 -0300

    Aula 01 Conteudo 04

---
+++ CursoPython#04.py
@@ -0,0 +1,15 @@
+print("Hello world!")
+
+print(7+4)
+print("7" + "4")
+print("Olá" , 5)
+
+nome = "JaoJao"
+idade = 29
+peso = 50
+print(nome, idade, peso)
+
+nome1 = input("Qual é a seu nome?")
+idade1 = input("Quantos anos você tem?")
+peso1 = input("Qual é o seu peso?")
+print(nome1, idade1, peso1)

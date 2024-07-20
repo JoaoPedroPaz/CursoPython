f538028c282a358b6b2ed401c8deda06e3096b4a
João Pedro Paz <77215433+JoaoPedroPaz@users.noreply.github.com>
2024-07-19 23:59:52 -0300

    Aula 04 Conteúdo 09

---
+++ Aulas/CursoPython#09.py
@@ -0,0 +1,8 @@
+frase="Curso em video Python"
+print(len(frase.strip()))
+frase=print(len(frase.replace("Python", "Android")))
+print(frase)
+print(frase.lower().find("curso"))
+dividido=frase.strip()
+print(dividido[0])
+print(dividido[2][3])
